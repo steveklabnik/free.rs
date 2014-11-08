74346e52de5aab814c2d3c83d1ab2665707b02cf
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-07 23:48:12 -0700

    Implement Bind; Expose safe API; Fix #1

--- src/trampoline/mod.rs
+++ src/trampoline/mod.rs
@@ -2,31 +2,51 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-// FIXME: make a macro for this
+use std::mem::{
+    transmute,
+};
 
-pub type F<'a,A> = proc():'a -> A;
+// Private API
 
-#[inline(always)]
-pub fn force<'a,A>(f:F<'a,A>) -> A {
-    f()
-}
+type F<'a,A> = proc():'a -> A;
 
-pub enum Free<'a,A> {
+enum Free<'a,A> {
     Pure(A),
     Roll(F<'a,Free<'a,A>>),
+    Bind(Box<Free<'a,*const u8>>, proc(*const u8):'a -> Free<'a,A>),
 }
 
 impl<'a,A> Free<'a,A> {
     #[inline]
-    pub fn resume(self) -> Result<A,F<'a,Free<'a,A>>> {
+    fn resume(self) -> Result<A,F<'a,Free<'a,A>>> {
         match self {
-            Pure(val) => { Ok (val) },
-            Roll(cmp) => { Err(cmp) },
+            Pure(val) => Ok (val),
+            Roll(thk) => Err(thk),
+            Bind(box mon2, con2) => {
+                match mon2 {
+                    Pure(val) => con2(val).resume(),
+                    Roll(thk1) => {
+                        Err({
+                            // FIXME: Without the annotation, rustc thinks we
+                            // need a Send bound. Maybe report upstream?
+                            let thk2: F<'a,Free<'a,A>> = proc() {
+                                Bind(box thk1(), con2)
+                            };
+                            thk2
+                        })
+                    },
+                    Bind(bmon1, con1) => {
+                        Bind(bmon1, proc(ptr1)
+                            Bind(box con1(ptr1), con2))
+                                .resume()
+                    },
+                }
+            },
         }
     }
 
     #[inline]
-    pub fn go(mut self, f:|F<'a,Free<'a,A>>| -> Free<'a,A>) -> A {
+    fn go(mut self, f:|F<Free<A>>| -> Free<A>) -> A {
         let res: A;
         loop { match self.resume() {
             Ok (val) => { res  = val; break },
@@ -34,6 +54,67 @@ impl<'a,A> Free<'a,A> {
         }};
         res
     }
+
+    #[inline]
+    fn run(self) -> A {
+        self.go(force)
+    }
+}
+
+#[inline(always)]
+fn coe_m<'a,A>(t:Trampoline<'a,A>) -> Free<'a,*const u8> {
+    let Trampoline(m) = t;
+    match m {
+        Pure(a) => Pure(unsafe { transmute(box a) }),
+        Roll(k) => Roll(proc() coe_m(Trampoline(k()))),
+        Bind(m,f) => Bind(m, proc(x) coe_m(Trampoline(f(x)))),
+    }
+}
+
+#[inline(always)]
+fn coe_f<'a,A,B>(k:proc(A):'a -> Trampoline<'a,B>) -> proc(*const u8):'a -> Free<'a,B> {
+    proc(x) {
+        let box a: Box<A> = unsafe { transmute(x) };
+        let Trampoline(mb) = k(a);
+        mb
+    }
+}
+
+// Public API
+
+pub type Thunk<'a,A> = proc():'a -> A;
+
+#[inline(always)]
+pub fn force<'a,A>(k:Thunk<'a,A>) -> A {
+    k()
+}
+
+pub struct Trampoline<'a,A>(Free<'a,A>);
+
+impl<'a,A> Trampoline<'a,A> {
+    #[inline(always)]
+    pub fn bind<A,B>(self, f:proc(A):'a -> Trampoline<'a,B>) -> Trampoline<'a,B> {
+        Trampoline(Bind(box coe_m(self), coe_f(f)))
+    }
+
+    #[inline]
+    pub fn run(self) -> A {
+        let Trampoline(m) = self;
+        m.go(force)
+    }
+}
+
+#[inline(always)]
+pub fn done<'a,A>(a:A) -> Trampoline<'a,A> {
+    Trampoline(Pure(a))
+}
+
+#[inline(always)]
+pub fn more<'a,A>(k:Thunk<'a,Trampoline<'a,A>>) -> Trampoline<'a,A> {
+    Trampoline(Roll(proc() {
+        let Trampoline(ma) = k();
+        ma
+    }))
 }
 
 #[cfg(test)]
@@ -42,18 +123,17 @@ mod tests {
 
     use self::num::BigInt;
     use super::{
-        Free,
-        Pure,
-        Roll,
-        force,
+        Trampoline,
+        done,
+        more,
     };
 
-    fn factorial<'a>(n:uint, acc:BigInt) -> Free<'a,BigInt> {
+    fn factorial<'a>(n:uint, acc:BigInt) -> Trampoline<'a,BigInt> {
         if n <= 2 {
-            Pure(acc)
+            done(acc)
         } else {
             let nb: BigInt = FromPrimitive::from_uint(n).unwrap();
-            Roll(proc() {
+            more(proc() {
                 factorial(n - 1, nb * acc)
             })
         }
@@ -62,7 +142,6 @@ mod tests {
     #[test]
     fn welp() {
         let acc: BigInt = FromPrimitive::from_uint(1u).unwrap();
-        println!("{}", factorial(1500, acc).go(force))
+        println!("{}", factorial(1500, acc).run())
     }
-
 }
