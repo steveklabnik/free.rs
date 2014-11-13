44f3c09a52f0102208308d0f5c52553a20d406af
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-12 17:46:19 -0700

    Remove wrapping; address overflows; fix #7
    There were a couple issues causing the overflows. First, resume was
    recursive but should have been a loop. This usually didn't cause a
    problem before because the recursion depth was never high enough.
    However, one of the coercions that bind called in turn called map,
    and map called bind again, leading to an infinite loop. This only
    occurred if Subs was used which is why the factorial example didn't
    trigger it.
    
    I also removed the struct wrapper but in hindsight I don't think that
    caused any problems (aside from maybe efficiency). I will probably add
    it back in the future.
    
    So the lesson is to be extra careful about which functions are
    called during the main loop. Some sort of call graph visualization
    would have helped uncover this sooner.

--- src/lib.rs
+++ src/lib.rs
@@ -10,70 +10,55 @@
 
 #[macro_export]
 macro_rules! free_monad(
-    ($Ty:ident, $S:ident, $smap:ident, [ $($ctx:ident,)* ]) =>
+    ($Free:ident, $S:ident, $smap:ident, [ $($ctx:ident,)* ]) =>
     {
-        struct Abs(*const u8);
-        type BFnOnce<'a, A, B> = Box<FnOnce<A, B> + 'a>;
+        pub struct Abs(*const u8);
+        pub type BFnOnce<'a, A, B> = Box<FnOnce<A, B> + 'a>;
 
-        enum Free<'a, $($ctx,)* X> {
+        pub enum $Free<'a, $($ctx,)* X> {
             Pure(X),
-            Roll($S<'a, $($ctx,)* Box<Free<'a, $($ctx,)* X>>>),
+            Roll($S<'a, $($ctx,)* Box<$Free<'a, $($ctx,)* X>>>),
             Subs(
-                BFnOnce<'a, (), Free<'a, $($ctx,)* Abs>>,
-                BFnOnce<'a, (Abs,), Free<'a, $($ctx,)* X>>,
+                BFnOnce<'a, (), $Free<'a, $($ctx,)* Abs>>,
+                BFnOnce<'a, (Abs,), $Free<'a, $($ctx,)* X>>,
             ),
         }
 
-        impl<'a $(,$ctx:'a)*> Free<'a, $($ctx,)* Abs> {
-            #[allow(dead_code)]
-            #[inline]
-            fn _map<Y:'a, F:'a>(self, f: BFnOnce<'a, (Abs,), Y>,) -> Free<'a, $($ctx,)* Y> {
-                self._bind(box move |:x| Pure(f.call_once((x,))))
-            }
-
+        impl<'a $(,$ctx:'a)*> $Free<'a, $($ctx,)* Abs> {
             // NOTE: keep this in sync with bind
             #[inline]
-            fn _bind<Y:'a>(self, f: BFnOnce<'a, (Abs,), Free<'a, $($ctx,)* Y>>) -> Free<'a, $($ctx,)* Y> {
+            fn _bind<Y:'a>(self, f: BFnOnce<'a, (Abs,), $Free<'a, $($ctx,)* Y>>) -> $Free<'a, $($ctx,)* Y> {
                 match self {
                     Subs(m, g) => Subs(m, box move |:x| Subs(box move |:| g.call_once((x,)), f)),
-                    _ => Subs(box move |:| { self }, f),
+                    _ => Subs(box move |:| self, f),
                 }
             }
         }
 
-        impl<'a $(,$ctx:'a)*, X:'a> Free<'a, $($ctx,)* X> {
-            #[inline(always)]
-            fn wrap(self) -> $Ty<'a, $($ctx,)* X> {
-                $Ty(self)
-            }
-
-            #[inline]
-            fn map<Y:'a, F:'a>(self, f: F) -> Free<'a, $($ctx,)* Y>
-                where
-                    F: FnOnce(X) -> Y,
-            {
-                self.bind(move |:x| Pure(f(x)))
-            }
-
+        impl<'a $(,$ctx:'a)*, X:'a> $Free<'a, $($ctx,)* X> {
             // NOTE: keep this in sync with _bind
             #[inline]
-            fn bind<Y:'a, F:'a>(self, f: F) -> Free<'a, $($ctx,)* Y>
+            pub fn bind<Y:'a, F:'a>(self, f: F) -> $Free<'a, $($ctx,)* Y>
                 where
-                    F: FnOnce(X) -> Free<'a, $($ctx,)* Y>,
+                    F: FnOnce(X) -> $Free<'a, $($ctx,)* Y>,
             {
                 // calls std::mem::transmute
                 #[inline(always)]
                 unsafe
-                fn lhs<'a $(,$ctx:'a)*, X:'a>(m: Free<'a, $($ctx,)* X>) -> Free<'a, $($ctx,)* Abs> {
-                    m.map(|:x| ::std::mem::transmute(box x))
+                fn lhs<'a $(,$ctx:'a)*, X:'a>(m: $Free<'a, $($ctx,)* X>) -> $Free<'a, $($ctx,)* Abs> {
+                    match m {
+                        Pure(a) => Pure(::std::mem::transmute(box a)),
+                        Roll(t) => Roll($smap(t, |:m2: Box<$Free<'a, $($ctx,)* _>> | box lhs(*m2))),
+                        Subs(m, f) => Subs(m, box move |:x| lhs(f.call_once((x,)))),
+                    }
                 }
 
                 // calls std::mem::transmute
                 #[inline(always)]
                 unsafe
-                fn rhs<'a $(,$ctx:'a)*, X:'a, Y:'a, F:'a>(f: F) -> BFnOnce<'a, (Abs,), Free<'a, $($ctx,)* Y>>
+                fn rhs<'a $(,$ctx:'a)*, X:'a, Y:'a, F:'a>(f: F) -> BFnOnce<'a, (Abs,), $Free<'a, $($ctx,)* Y>>
                     where
-                        F: FnOnce(X) -> Free<'a, $($ctx,)* Y>,
+                        F: FnOnce(X) -> $Free<'a, $($ctx,)* Y>,
                 {
                     box move |:ox| f.call_once((*::std::mem::transmute::<_, Box<_>>(ox),))
                 }
@@ -88,137 +73,31 @@ macro_rules! free_monad(
             }
 
             #[inline]
-            fn fold<Y, P, R>(self, p: P, r: R) -> Y
-                where
-                    P: Fn(X) -> Y,
-                    R: Fn($S<'a, $($ctx,)* Box<Free<'a, $($ctx,)* X>>>) -> Y,
-            {
-                match self.resume() {
-                    Ok (a) => p.call_once((a,)),
-                    Err(t) => r.call_once((t,)),
-                }
-            }
-
-            #[inline]
-            fn resume(self) -> Result<X, $S<'a, $($ctx,)* Box<Free<'a, $($ctx,)* X>>>> {
-                match self {
-                    Pure(a) => Ok (a),
-                    Roll(t) => Err(t),
+            fn resume(mut self) -> Result<X, $S<'a, $($ctx,)* Box<$Free<'a, $($ctx,)* X>>>> {
+                loop { match self {
+                    Pure(a) => return Ok (a),
+                    Roll(t) => return Err(t),
                     Subs(ma, f) => {
                         match ma.call_once(()) {
-                            Pure(a) => f.call_once((a,)).resume(),
-                            Roll(t) => Err($smap(t, move |:m:Box<Free<'a, _>> | box m._bind(f))),
-                            Subs(mb, g) => mb.call_once(())._bind(box move |:pb| g.call_once((pb,))._bind(f)).resume(),
+                            Pure(a) => { self = f.call_once((a,)) },
+                            Roll(t) => return Err($smap(t, move |:m:Box<$Free<'a, $($ctx,)* _>> | box m._bind(f))),
+                            Subs(mb, g) => { self = mb.call_once(())._bind(box move |:pb| g.call_once((pb,))._bind(f)) },
                         }
                     },
-                }
+                }}
+                // self = Subs(mb, box move |:pb| Subs(box move |:| g.call_once((pb,)), f));
             }
 
-            #[inline]
-            fn bounce<F>(self, f: F) -> Free<'a, $($ctx,)* X>
-                where
-                    F: FnOnce($S<'a, $($ctx,)* Box<Free<'a, $($ctx,)* X>>>) -> Free<'a, $($ctx,)* X>,
-            {
-                match self.resume() {
-                    Ok (a) => Pure(a),
-                    Err(t) => f.call_once((t,)),
-                }
-            }
-
-            #[allow(dead_code)]
             #[inline]
             fn go<F>(mut self, f: F) -> X
                 where
                     // f must be a Fn since we may call it many times
-                    F: Fn($S<'a, $($ctx,)* Box<Free<'a, $($ctx,)* X>>>) -> Free<'a, $($ctx,)* X>,
+                    F: Fn($S<'a, $($ctx,)* Box<$Free<'a, $($ctx,)* X>>>) -> $Free<'a, $($ctx,)* X>,
             {
-                let acc: X;
                 loop { match self.resume() {
-                    Ok (a) => { acc  = a; break     },
+                    Ok (a) => return a,
                     Err(t) => { self = f.call((t,)) },
-                }};
-                acc
-            }
-        }
-
-        pub struct $Ty<'a $(,$ctx:'a)*, X:'a>(Free<'a, $($ctx,)* X>);
-
-        impl<'a $(,$ctx:'a)*, X:'a> $Ty<'a, $($ctx,)* X> {
-            #[inline(always)]
-            fn proj(self) -> Free<'a, $($ctx,)* X> {
-                let $Ty(mx) = self; mx
-            }
-
-            #[inline]
-            pub fn map<Y:'a, F:'a>(self, f: F) -> $Ty<'a, $($ctx,)* Y>
-                where
-                    F: FnOnce(X) -> Y,
-            {
-                self.proj().map(f).wrap()
-            }
-
-            // NOTE: keep this in sync with _bind
-            #[inline]
-            pub fn bind<Y:'a, F:'a>(self, f: F) -> $Ty<'a, $($ctx,)* Y>
-                where
-                    F: FnOnce(X) -> $Ty<'a, $($ctx,)* Y>,
-            {
-                self.proj()
-                    .bind(move |:x| f(x).proj())
-                    .wrap()
-            }
-
-            #[inline]
-            pub fn fold<Y, P, R>(self, p: P, r: R) -> Y
-                where
-                    P: Fn(X) -> Y,
-                    R: Fn($S<'a, $($ctx,)* Box<$Ty<'a, $($ctx,)* X>>>) -> Y,
-            {
-                self.proj()
-                    .fold(p, |&:sbmx:$S<'a, _>| {
-                        r($smap(sbmx, |:bmx:Box<Free<'a, _>>| {
-                            box bmx.wrap()
-                        }))
-                    })
-            }
-
-            #[inline]
-            pub fn resume(self) -> Result<X, $S<'a, $($ctx,)* Box<$Ty<'a, $($ctx,)* X>>>> {
-                self.proj()
-                    .resume()
-                    .map_err(|sbmx| {
-                        $smap(sbmx, |:bmx:Box<Free<'a, _>>| {
-                            box bmx.wrap()
-                        })
-                    })
-            }
-
-            #[inline]
-            pub fn bounce<F>(self, f: F) -> $Ty<'a, $($ctx,)* X>
-                where
-                    F: FnOnce($S<'a, $($ctx,)* Box<$Ty<'a, $($ctx,)* X>>>) -> $Ty<'a, $($ctx,)* X>,
-            {
-                self.proj()
-                    .bounce(move |:sbmx| {
-                        f($smap(sbmx, |:bmx:Box<Free<'a, _>>| {
-                            box bmx.wrap()
-                        })).proj()
-                    }).wrap()
-            }
-
-            #[allow(dead_code)]
-            #[inline]
-            pub fn go<F>(self, f: F) -> X
-                where
-                    // f must be a Fn since we may call it many times
-                    F: Fn($S<'a, $($ctx,)* Box<$Ty<'a, $($ctx,)* X>>>) -> $Ty<'a, $($ctx,)* X>,
-            {
-                self.proj()
-                    .go(|&:sbmx| {
-                        f($smap(sbmx, |:bmx:Box<Free<'a, _>>| {
-                            box bmx.wrap()
-                        })).proj()
-                    })
+                }}
             }
         }
 
