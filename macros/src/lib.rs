390dd50b939e7c1d784a73c98abe8a16cb87f3fa
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-19 10:21:13 -0700

    Overloaded_calls is included with unboxed_closures

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -5,7 +5,6 @@
 #![doc(html_root_url = "http://www.rust-ci.org/epsilonz/free.rs/doc/free/")]
 
 #![feature(macro_rules)]
-#![feature(overloaded_calls)]
 #![feature(unboxed_closures)]
 
 pub mod free;
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 #![license = "MIT"]
 #![doc(html_root_url = "http://www.rust-ci.org/epsilonz/free.rs/doc/free/")]
 
-#![feature(overloaded_calls)]
 #![feature(phase)]
 #![feature(unboxed_closures)]
 
